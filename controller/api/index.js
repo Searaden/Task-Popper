7b3f0128db038855624d96c2eff70720cf29489f
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-07-19 19:17:54 -0700

    merging timelines

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,13 +1,7 @@
-const router = require('express').Router();
 
+const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const bountyRoutes = require('./bountyRoutes');
-const taskRoutes = require('./taskRoutes');
-const subtaskRoutes = require('./subtaskRoutes');
 
 router.use('/user', userRoutes);
-router.use('/bounty', bountyRoutes);
-router.use('/task', taskRoutes);
-router.use('/subtask', subtaskRoutes);
 
 module.exports = router;
