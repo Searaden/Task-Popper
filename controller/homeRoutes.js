d6e35245cdfe956ab1c6e2cda0ea96278e486070
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-07-19 21:36:13 -0700

    semi- working css/jss

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { Task } = require('../model')
 
 router.get('/', async (req, res) =>{
     try{
-        // on load content
-        res.render('test')
+        const taskData = await Task.findAll();
+        const tasks = taskData.map((task)=> task.get({plain: true}))
+
+        res.render('homepage',{tasks});
     }catch(err){
         res.status(500).json(err);
     }
