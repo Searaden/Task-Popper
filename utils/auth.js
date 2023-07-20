28a3dd44e55351cf7befead428328bfe5a16ba07
joel Quezada <halo.joel.quezada@gmail.com>
2023-07-19 17:52:21 -0700

    user

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+router.get('/dashboard', withAuth, (req, res) => {
+  // Only authenticated users can reach this route
+  // Use req.session.user to access user data
+});
+
+// More routes...
+
+module.exports = router;
